3e735d5004063bfd684b746eb63b1a104bfb82be
tejal tari <tari.tejal.13ce1096@gmail.com>
2016-07-24 10:12:38 +0530

    menteenindex changes

--- menteeindex.php
+++ menteeindex.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>KATALYST / Non-profit responsive Bootstrap HTML5 template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Dosis:400,700' rel='stylesheet' type='text/css'>
+
+        <!-- Bootsrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <!-- Owl carousel -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+
+        <!-- Template main Css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <!-- Modernizr -->
+        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+
+
+    </head>
 <?php
 session_start();
 ?>
@@ -59,7 +87,53 @@ $conn = new mysqli($host,$username,$password,$database);
 	?>
 	</head>
 	<body>
-	
+	<nav class="navbar navbar-static-top">
+
+            
+            <div class="navbar-main">
+              
+              <div class="container">
+
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                  </button>
+                  
+                  <a class="navbar-brand" href="index.html"><img style="max-height:35px; margin-top: -3px;margin-left: 0px;background-color:" src="image/logo1.png"></a>
+                  
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse pull-right">
+
+                  <ul class="nav navbar-nav">
+
+                    <li><a class="is-active" href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+
+                    <li><a href="index.html">LOGOUT</a></li>
+                    <?php
+	  if($_SESSION['role']=='Mentor')
+	  echo'<li><a href="profile.php">PROFILE</a></li>';
+	  else
+	  echo'<li><a href="profile-mentee.php">PROFILE</a></li>';
+	 ?>
+
+                  </ul>
+
+                </div> <!-- /#navbar -->
+
+              </div> <!-- /.container -->
+              
+            </div> <!-- /.navbar-main -->
+
+
+        </nav> 
+
 	
 	  <header>
 	  <?php
@@ -68,7 +142,6 @@ $conn = new mysqli($host,$username,$password,$database);
 	  else
 	  echo'<p id="p2">Mentee DashBoard</p>';
 	 ?>
-	 <form action="menteeindex.php" method="post"><input type="submit" name="log" value="Logout" style="float: right;margin-right: 50px"></button></form>
 	 </header>
 	 <div id="progress">
 	 <body>
@@ -96,15 +169,6 @@ $conn = new mysqli($host,$username,$password,$database);
 			 <input type="submit" value="submit" name="submit"id="submit"></br></br>
 	 </form>
 	 </div>
-	 
-	 <nav id="menu3">
-	 <?php
-	  if($_SESSION['role']=='Mentor')
-	  echo'<a href="profile.php">Profile</a>';
-	  else
-	  echo'<a href="profile-mentee.php">Profile</a>';
-	 ?>
-     </nav>
 	 
 	 <div id="menu4">
 	 <h1 style="float:right;margin-right:200px">Upcoming Meetings:</h1></br></br>
